1d353e4c5b845fec6ceed777117cd9275f88c8d9
pjmenezes <pmenezes1hp@gmail.com>
2021-05-26 14:07:18 -0400

    first commit

---
+++ public/web.js/index.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBw_Vf6vSU9Qq2ONru51Au50STXOjeV8-4",
+    authDomain: "solucao-1e334.firebaseapp.com",
+    projectId: "solucao-1e334",
+    storageBucket: "solucao-1e334.appspot.com",
+    messagingSenderId: "167312473722",
+    appId: "1:167312473722:web:ca255970f8d5dc8b7405c3",
+    measurementId: "G-2910FV5HTH"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
